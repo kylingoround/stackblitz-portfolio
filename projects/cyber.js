b248661d8a95c8e71418bde33a5050d7d067dca0
kylingoround <kylingoround@gmail.com>
2019-08-18 22:57:45 -0400

    1 file modified

--- projects/cyber.js
+++ projects/cyber.js
@@ -4,7 +4,9 @@ import styled from 'styled-components'
 
 import Nav from '../components/nav'
 
-const Banner = styled.img``
+const Banner = styled.img`
+  max-height:200px;
+`
 const PageTitle = styled.div``
 const H1 = styled.div`` // section title
 const H2 = styled.div`` // subtitle
@@ -16,8 +18,7 @@ const NarrowImg = styled.img``
 
 const Content = () =>
 <>
-<Banner src='https://paper.dropbox.com/ep/redirect/image?url=https%3A%2F%2Fpaper-attachments.dropbox.com%2Fs_9FCB84EFBC580CB5605114E61EA103B6902650A4F0886287613E54B90E99A86C_1566180044311_image.png&hmac=%2FD%2FHU0EbHDi7o%2B3ZDlDC5S%2BGXuz%2FxLBfZLsqKtbbAd0%3D'/>
-
+<Banner src='https://uc612a504ce1cd22da5c4b2ea87b.previews.dropboxusercontent.com/p/thumb/AAh2p7va_u3bQGwAriN0fJJqVFyRVUTGOZD560A8YGfDf3ShGcZf-v6lnOSlGvnKXJ-dRmGGp_ukIHfiahtxnSWWSNTg9N13rAbIqh6JQ4sjPcFOt3JAqSDzE4OKz33mEqwC9PsVW5TYX9jTxNWju0XwxI7tloFF6kh7qwIbP0CSaECwurLqQS4bQHyJBx_Cr6p2hH0oubgUcF7iCmFuiy0oJb7Yf68HOINn4RsXHNLhlVPVdVBjElfAYTxM59VcOK2mqre-fwf5R5cDg1-iYgD7UmbORrcSAaX-bK6yYWSUj7bQ2dkhgfJcqNjfPRER9kL6Fft27-FrCooiz8T58idZGeqSwRXtU0UPUW-DTXclRKwVf7SBETf4Fq-W7ITYFEtZhxfTJSKNqVuNNLGaSMAv/p.png?fv_content=true&size_mode=5'/>
 <div>Building Transparent and Trustable A.I. Dashboard for Cybersecurity Operators</div>
 </>
 
